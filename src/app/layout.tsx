66da280103c0916faa6f687f3fcd9e98a3826c65
ESS-Salem <ESS@DESKTOP-QVAQUM3>
2025-10-30 08:58:38 -0300

    beta funcional pre-comit de ContactForm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,18 +44,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* <main id="contenido">{children}</main> */}
 
         {/* GTM (Google Tag Manager) */}
-        {process.env.NEXT_PUBLIC_GTM_ID && (
+        {/* {process.env.NEXT_PUBLIC_GTM_ID && (
           <>
             {/* Head script (migrated to next/script) */}
-            <Script id="gtm-init" strategy="afterInteractive">
+            {/* <Script id="gtm-init" strategy="afterInteractive">
               {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`}
-            </Script>
+            </Script> */}
             {/* Body no-script fallback */}
-            <noscript>
+            {/* <noscript>
               <iframe
                 src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
                 height="0"
@@ -64,7 +64,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               />
             </noscript>
           </>
-        )}
+        )} */}
 
         {children}
       </body>
