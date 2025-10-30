8037cae8477f52baaa59aec9b5993d3c68a26fd6
Seilem94 <salemhidd1994@gmail.com>
2025-10-30 02:43:34 -0300

    refactor: remove NavBar component and clean up layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { site } from "@/shared/config/site"
 import Script from "next/script"
 import "./globals.css";
-import NavBar from "@/components/NavBar" // 👈 importa tu NavBar
 
 export const metadata: Metadata = {
   metadataBase: new URL(site.url),
@@ -27,9 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es" >
       <body className="antialiased">
-        {/* 🌐 Navbar visible en todas las páginas */}
-        {/* <NavBar />         */}
-        
+
         {/* 📄 Contenido principal */}
         {/* <main id="contenido">{children}</main> */}
 
